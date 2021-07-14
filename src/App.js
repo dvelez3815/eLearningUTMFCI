e532cc16d23cdb798a63d855b957b8cfb7f53ecf
Nicromano <ljose297@gmail.com>
2021-07-14 00:09:24 -0500

    rutas agregadas

--- src/App.js
+++ src/App.js
@@ -1,89 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {NavComponent} from './components/NavComponent';
-import { ModuleProgress } from './components/ModuleProgress';
-import Activity from './components/Activities/Activity';
-import i_writting from './assets/icons/teacher.png'
-import { useEffect } from 'react';
-import Activities from './components/Activities/Activities';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 
+/* Pages */
+import ActivitiesPage from './pages/ActivitiesPage/ActivitiesPage'
+import HomePage from './pages/HomePage/HomePage';
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+          <Route exact path="/activities" component={ActivitiesPage}></Route>
+          <Route exact path="/" component={HomePage}></Route>
 
-
-      <div className="container  w-6/12">
-      {/* <NavComponent logo={logo}></NavComponent> */}
-      <ModuleProgress moduleName="Modulo 1" percent={100}></ModuleProgress>
-      {/* <Activities modules={libro_ingles.modulos}></Activities> */}
-      <div className="grid grid-cols-6">
-      <Activity img={i_writting} percent={100} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={100} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={100} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={100} name={"Grammar"} colspan={'col-span-2 col-start-2'}/>
-      <Activity img={i_writting} percent={100} name={"Grammar"} colspan={'col-span-2'}/>
       </div>
-      </div>
-
-
-
-
-
-
-      <div className="container  w-6/12">
-      {/* <NavComponent logo={logo}></NavComponent> */}
-      <ModuleProgress moduleName="Modulo 1" percent={70}></ModuleProgress>
-      {/* <Activities modules={libro_ingles.modulos}></Activities> */}
-      <div className="grid grid-cols-6">
-      <Activity img={i_writting} percent={70} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={70} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={70} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={70} name={"Grammar"} colspan={'col-span-2 col-start-2'}/>
-      <Activity img={i_writting} percent={70} name={"Grammar"} colspan={'col-span-2'}/>
-      </div>
-      </div>
-
-
-      <div className="container  w-6/12">
-      {/* <NavComponent logo={logo}></NavComponent> */}
-      <ModuleProgress moduleName="Modulo 2" percent={69}></ModuleProgress>
-      {/* <Activities modules={libro_ingles.modulos}></Activities> */}
-      <div className="grid grid-cols-6">
-      <Activity img={i_writting} percent={69} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={69} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={69} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={69} name={"Grammar"} colspan={'col-span-2 col-start-2'}/>
-      <Activity img={i_writting} percent={69} name={"Grammar"} colspan={'col-span-2'}/>
-      </div>
-      </div>
-
-      <div className="container  w-6/12">
-      {/* <NavComponent logo={logo}></NavComponent> */}
-      <ModuleProgress moduleName="Modulo 3" percent={49}></ModuleProgress>
-      {/* <Activities modules={libro_ingles.modulos}></Activities> */}
-      <div className="grid grid-cols-6">
-      <Activity img={i_writting} percent={49} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={49} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={49} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={49} name={"Grammar"} colspan={'col-span-2 col-start-2'}/>
-      <Activity img={i_writting} percent={49} name={"Grammar"} colspan={'col-span-2'}/>
-      </div>
-      </div>
- 
-      <div className="container  w-6/12">
-      {/* <NavComponent logo={logo}></NavComponent> */}
-      <ModuleProgress moduleName="Modulo 4" percent={21}></ModuleProgress>
-      {/* <Activities modules={libro_ingles.modulos}></Activities> */}
-      <div className="grid grid-cols-6">
-      <Activity img={i_writting} percent={21} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={21} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={21} name={"Grammar"} colspan={'col-span-2'}/>
-      <Activity img={i_writting} percent={21} name={"Grammar"} colspan={'col-span-2 col-start-2'}/>
-      <Activity img={i_writting} percent={21} name={"Grammar"} colspan={'col-span-2'}/>
-      </div>
-      </div>                  
-
-    </div>
+    </Router>
+    
   );
 }
 
