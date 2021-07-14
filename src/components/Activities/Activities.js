cb828a77ebe9cf4e584df9b08f0123e1d9aac572
xaviermaster <xavier.velez3132@gmail.com>
2021-07-13 23:12:58 -0500

    mensajes

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -3,21 +3,14 @@ import Activity from "./Activity";
 import algo from '../../assets/icons/teacher.png'
 
 export default function Activities(props){
-    let images = [];
+    
     return(
-        <div>
-            {props.modules.map((englishModule,index)=>{
-                // img[0] = require(e.imagenes[index])
-                englishModule.imagenes.map((image, index)=>{
-                    let t2 = String(image)
-                    // images[index] = require(t2);
-                    console.log(t2);
-                })                
-                
-                return <Activity img={algo} percent={1}/>
-            })}
-        </div>
-        
+        <>
+        {/* 
+            Aquí se deben de crear componentes Activity en base a la data que de la API 
+            
+        */}
+        </>
         
     );
 }
