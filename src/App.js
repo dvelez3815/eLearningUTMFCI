8eaf549183213193f608143c91ce995b66af03b0
xaviermaster <xavier.velez3132@gmail.com>
2021-07-19 16:56:36 -0500

    pruebas de vista grammar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavComponent} from './components/NavComponent';
-import { ModuleProgress } from './components/ModuleProgress';
-import Activity from './components/Activities/Activity';
-import i_writting from './assets/icons/teacher.png'
-import { useEffect } from 'react';
-import Activities from './components/Activities/Activities';
 import EModule from './components/EModules/EModule';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Grammar from './components/pages/ejercicios/Grammar';
--- src/components/pages/ejercicios/Grammar.js
+++ src/components/pages/ejercicios/Grammar.js
@@ -1,142 +1,106 @@
+import "./CheckExercise.css";
+
 const Grammar = (props) => {
-  let ejercicio = {
-    Exercisetype: "Grammar",
-    type: "MatchIW",
-    images: [
-      "https://mercadilloecuador.com/wp-content/uploads/2020/07/IMG-9697.png",
-      "https://mercadilloecuador.com/wp-content/uploads/2020/07/IMG-9697.png",
-      "https://mercadilloecuador.com/wp-content/uploads/2020/07/IMG-9697.png",
-    ],
-    setOfWords: [
-      "It’s twelve o’clock",
-      "It’s one o’clock",
-      "It’s four o’clock",
-    ],
-    correctSet: [
-      "It’s four o’clock",
-      "It’s twelve o’clock",
-      "It’s one o’clock",
-    ],
-  }
+  let datos = {
+    total_completado: 80,
+  };
   return (
-    <div className="grid grid-row-2">
-      <div>
-        <div
-          aria-valuemax="1"
-          aria-valuemin="0"
-          aria-valuenow="0"
-          className="_2YmyD _2_zk1"
-          role="progressbar"
-          
-        >
-          <div className="-oI84" style={{opacity: "0", width: "0%"}}>
-            <div className="KAjTN"></div>
-            <div className="fSJFz">
-              <div style={{height: "16px", width: "16px"}}>
-                <span className="_3a8EI _1NSc8">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 225 230"
-                    width="225"
-                    height="230"
-                    preserveAspectRatio="xMidYMid meet"
-                    style={{width: "100%", height: "100%", transform: "translate3d(0px, 0px, 0px)"}}
-                  >
-                    <defs>
-                      <clipPath id="__lottie_element_88">
-                        <rect width="225" height="230" x="0" y="0"></rect>
-                      </clipPath>
-                      <clipPath id="__lottie_element_92">
-                        <path d="M0,0 L828,0 L828,1472 L0,1472z"></path>
-                      </clipPath>
-                      <clipPath id="__lottie_element_97">
-                        <path d="M0,0 L828,0 L828,1472 L0,1472z"></path>
-                      </clipPath>
-                      <clipPath id="__lottie_element_102">
-                        <path d="M0,0 L828,0 L828,1472 L0,1472z"></path>
-                      </clipPath>
-                    </defs>
-                    <g clip-path="url(#__lottie_element_88)">
-                      <g
-                        clip-path="url(#__lottie_element_102)"
-                        transform="matrix(1.399999976158142,0,0,1.399999976158142,-101.0999755859375,-895.4000244140625)"
-                        opacity="0.61"
-                        style={{display: "block"}}
-                      >
-                        <g
-                          transform="matrix(0.3799985647201538,0,0,0.3799985647201538,139,712.2915649414062)"
-                          opacity="0.9999982798675414"
-                          
-                        >
-                          <path
-                            fill="rgb(120,200,0)"
-                            fill-opacity="1"
-                            d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z"
-                          ></path>
-                        </g>
-                      </g>
-                      <g
-                        clip-path="url(#__lottie_element_97)"
-                        style={{display: "block"}}
-                        transform="matrix(-1.2852754592895508,0.6268706917762756,-0.6268706917762756,-1.2852754592895508,791.97119140625,934.405517578125)"
-                        opacity="1"
-                      >
-                        <g
-                          transform="matrix(0.08947759866714478,0,0,0.08947759866714478,154,769)"
-                          opacity="0.01918743640105838"
-                          style={{display: "block"}}
-                        >
-                          <path
-                            fill="rgb(120,200,0)"
-                            fill-opacity="1"
-                            d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z"
-                          ></path>
-                        </g>
-                      </g>
-                      <g
-                        clip-path="url(#__lottie_element_92)"
-                        transform="matrix(1.350000023841858,0,0,1.350000023841858,-89.4000244140625,-880.6000366210938)"
-                        opacity="1"
-                        style={{display: "block"}}
-                      >
-                        <g
-                          transform="matrix(0.15484324097633362,0,0,0.15484324097633362,126.86371612548828,747.3348999023438)"
-                          opacity="0.9164718425370612"
-                          
-                        >
-                          <path
-                            fill="rgb(120,200,0)"
-                            fill-opacity="1"
-                            d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z"
-                          ></path>
-                        </g>
-                      </g>
-                      <g
-                        transform="matrix(1.8300000429153442,0,0,1.8300000429153442,116.5,108)"
-                        opacity="0.37882117484129946"
-                        style={{display: "block"}}
-                      >
-                        <path
-                          stroke-linecap="butt"
-                          stroke-linejoin="miter"
-                          fill-opacity="0"
-                          stroke-miterlimit="4"
-                          stroke="rgb(120,200,0)"
-                          stroke-opacity="1"
-                          stroke-width="0"
-                          d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z"
-                        ></path>
-                      </g>
-                    </g>
-                  </svg>
-                </span>
-              </div>
-            </div>
-          </div>
+    <div className="container m-auto p-auto w-10/12">
+      <div className="flex justify-between py-5">
+        <h2>
+          <span>
+            <p className="text-lg font-bold	">¿Cuál de estos es agua?</p>
+          </span>
+        </h2>
+        <div>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-    </div>
-  )
-}
+      <div className="flex flex-wrap items-center justify-center	gap-2 mb-4" aria-label="choice" role="radiogroup">
+        <div className="flex flex-col w-32 flex-wrap md:w-1/5 lg:w-1/4 cardCheck" aria-checked="false" role="radio" tabIndex="-1" data-test="challenge-choice-card">
+          <img
+            src="https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
+            alt=""
+          />
+          <p className="text-sm font-medium	">Tea</p>
+        </div>
+        <div className="flex flex-col w-32 flex-wrap md:w-1/5 lg:w-1/4 cardCheck" aria-checked="false" role="radio" tabIndex="-1" data-test="challenge-choice-card">
+          <img
+            src="https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
+            alt=""
+          />
+          <p className="text-sm font-medium	">Tea</p>
+        </div>
+        <div className="flex flex-col w-32 flex-wrap md:w-1/5 lg:w-1/4 cardCheck" aria-checked="false" role="radio" tabIndex="-1" data-test="challenge-choice-card">
+          <img
+            src="https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
+            alt=""
+          />
+          <p className="text-sm font-medium	">Tea</p>
+        </div>
+        <div className="flex flex-col w-32 flex-wrap md:w-1/5 lg:w-1/4 cardCheck" aria-checked="false" role="radio" tabIndex="-1" data-test="challenge-choice-card">
+          <img
+            src="https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
+            alt=""
+          />
+          <p className="text-sm font-medium	">Tea</p>
+        </div>
+        <div className="flex flex-col w-32 flex-wrap md:w-1/5 lg:w-1/4 cardCheck" aria-checked="true" role="radio" tabIndex="-1" data-test="challenge-choice-card">
+          <img
+            src="https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
+            alt=""
+          />
+          <p className="text-sm font-medium	">Tea</p>
+        </div>
+      </div>
 
-export default Grammar
+      <div className="flex justify-between flex-col sm:flex-row">
+        <div className="mb-4">
+          <button className="btn-exercise">
+            <span>
+              <p>saltar</p>
+            </span>
+          </button>
+        </div>
+        <div className="mb-4">
+        <button className="bg-green-500 hover:bg-green-700 btn-exercise" disabled={"True"}>
+          <span>
+            <p>comprobar</p>
+          </span>
+          </button>
+        </div>
+      </div>
+
+      <div className="flex justify-between">
+        <div></div>
+        <div className="order-last">
+          <p>COMPLETADO: {`${datos.total_completado}%`}</p>
+        </div>
+      </div>
+
+      <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-amber-200 h-4 border">
+        <div
+          style={{ width: `${datos.total_completado}%` }}
+          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-400"
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Grammar;
