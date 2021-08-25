208069e2e66fd4a14dae5d7941721d86d237d424
Nicromano <ljose297@gmail.com>
2021-08-24 23:16:05 -0500

    profile

--- src/components/pages/inicio/Inicio.js
+++ src/components/pages/inicio/Inicio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavComponent } from "../../NavComponent";
-import logo from "../../../assets/icons/medalla.svg";
+import logo from "../../../assets/resource/Logo_Provicional.ico";
 import EModule from "../../EModules/EModule";
 import StarIcon from '@material-ui/icons/Star';
 import CollectionsBookmarkIcon from '@material-ui/icons/CollectionsBookmark';
