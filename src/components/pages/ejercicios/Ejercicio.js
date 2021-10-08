1f55a89a8e3b9d0617f46223bb4168f0331663c1
Nicromano <ljose297@gmail.com>
2021-10-07 21:26:25 -0500

    imagenes

--- src/components/pages/ejercicios/Ejercicio.js
+++ src/components/pages/ejercicios/Ejercicio.js
@@ -1,100 +1,158 @@
-import React, {useRef } from 'react'
-import ProgressBar from './ProgressBar'
+import React, { useRef } from "react";
+import ProgressBar from "./ProgressBar";
 
 //load ejercicio.css
-import './Ejercicio.css'
-import EjercicioFooter from './EjercicioFooter'
-import { OpcionCorrecta_1 } from './OpcionCorrecta_1'
-import { OpcionCorrecta_n } from './OpcionCorrecta_n'
-import Arrastrar from './Arrastrar'
-import VerdaderoFalso from './VerdaderoFalso'
-import CompletarTexto from './CompletarTexto'
-import Emparejar from './Emparejar'
-import EjercicioFooterPruebaLibros from './EjercicioFooterPruebaLibros'
-
+import "./Ejercicio.css";
+import EjercicioFooter from "./EjercicioFooter";
+import { OpcionCorrecta_1 } from "./OpcionCorrecta_1";
+import { OpcionCorrecta_n } from "./OpcionCorrecta_n";
+import Arrastrar from "./Arrastrar";
+import VerdaderoFalso from "./VerdaderoFalso";
+import CompletarTexto from "./CompletarTexto";
+import Emparejar from "./Emparejar";
+import EjercicioFooterPruebaLibros from "./EjercicioFooterPruebaLibros";
 
 export const Ejercicio = (props) => {
+  const [juego, setJuego] = React.useState([]);
+  const [cargado, setCargado] = React.useState(true);
+  const [finJuego, setFinJuego] = React.useState(false);
+  const [contadorRespondidas, setContadorRespondidas] = React.useState(0);
+  const [preguntasValidas, setPreguntasValidas] = React.useState(0);
+  const [aciertos, setAciertos] = React.useState(0);
 
-    const [juego, setJuego] = React.useState([]);
-    const [cargado, setCargado] = React.useState(true);
-    const [finJuego, setFinJuego] = React.useState(false);
-    const [contadorRespondidas, setContadorRespondidas] = React.useState(0);
-    const [preguntasValidas, setPreguntasValidas] = React.useState(0);
-    const [aciertos, setAciertos] = React.useState(0);
+  const panelJuego = useRef(null);
 
-    const panelJuego = useRef(null);
-    
+  React.useEffect(() => {
+    // const cargarVista = async() => {
+    //     await cargarEjercicios(juego, setJuego, panelJuego, setCargado, finJuego, ejercicios);
+    // }
+    if (juego.length === 0) {
+      cargarEjercicios2(
+        props.ejercicios,
+        setJuego,
+        panelJuego,
+        setCargado,
+        preguntasValidas,
+        setPreguntasValidas
+      );
+    }
+  }, []); //cuando haya un cambio de pregunta se actualiza el estado del componente.
 
-    React.useEffect(() => {
-        // const cargarVista = async() => {
-        //     await cargarEjercicios(juego, setJuego, panelJuego, setCargado, finJuego, ejercicios);
-        // }
-        if(juego.length === 0){
-            cargarEjercicios2(props.ejercicios, setJuego, panelJuego,setCargado,preguntasValidas,setPreguntasValidas);
-        }
-        
-
-    }, []) //cuando haya un cambio de pregunta se actualiza el estado del componente.
-
-
-    return (
-        <div className={"ejercicio"}>
-            
-            {cargado || <ProgressBar totalEjercicios = {preguntasValidas} resueltos ={contadorRespondidas} contadorRespondidas={contadorRespondidas} />}
-            {finJuego? <h2 className="container m-auto p-auto w-6/12">Fin del juego...</h2>:juego[juego.length-1]}
-            {/* Eejercicio footer se encarga de verificar mediante el botoòn de comprobar la respuesta correcta.
+  return (
+    <div className={"ejercicio"}>
+      {cargado || (
+        <ProgressBar
+          totalEjercicios={preguntasValidas}
+          resueltos={contadorRespondidas}
+          contadorRespondidas={contadorRespondidas}
+        />
+      )}
+      {finJuego ? (
+        <h2 className="container m-auto p-auto w-6/12">Fin del juego...</h2>
+      ) : (
+        juego[juego.length - 1]
+      )}
+      {/* Eejercicio footer se encarga de verificar mediante el botoòn de comprobar la respuesta correcta.
             Para esto es necesario enviar el juego actual es decir juego[juego.length-1]	y el panel del juego actual, es decir lo que està en el medio de la pantalla, que es el juego actual.
             Tambièn se debe de enviar el contador para ir incrementando el estado de respondidas. */}
-        
 
-            
-            {finJuego || props.esPrueba?<EjercicioFooterPruebaLibros totalEjercicios = {preguntasValidas} ejercicio={juego[juego.length-1]} juego={juego} setJuego = {setJuego} cargado={cargado} setCargado={setCargado} setFinJuego={setFinJuego} miref={panelJuego} contadorRespondidas={contadorRespondidas} setContadorRespondidas={setContadorRespondidas} aciertos={aciertos} setAciertos={setAciertos}/>:<EjercicioFooter ejercicio={juego[juego.length-1]} juego={juego} setJuego = {setJuego} cargado={cargado} setCargado={setCargado} setFinJuego={setFinJuego} miref={panelJuego} contadorRespondidas={contadorRespondidas} setContadorRespondidas={setContadorRespondidas}/>}
-        </div>
-    )
-}
+      {finJuego || props.esPrueba ? (
+        <EjercicioFooterPruebaLibros
+          totalEjercicios={preguntasValidas}
+          ejercicio={juego[juego.length - 1]}
+          juego={juego}
+          setJuego={setJuego}
+          cargado={cargado}
+          setCargado={setCargado}
+          setFinJuego={setFinJuego}
+          miref={panelJuego}
+          contadorRespondidas={contadorRespondidas}
+          setContadorRespondidas={setContadorRespondidas}
+          aciertos={aciertos}
+          setAciertos={setAciertos}
+        />
+      ) : (
+        <EjercicioFooter
+          ejercicio={juego[juego.length - 1]}
+          juego={juego}
+          setJuego={setJuego}
+          cargado={cargado}
+          setCargado={setCargado}
+          setFinJuego={setFinJuego}
+          miref={panelJuego}
+          contadorRespondidas={contadorRespondidas}
+          setContadorRespondidas={setContadorRespondidas}
+        />
+      )}
+    </div>
+  );
+};
 
-const cargarEjercicios2 = (ejercicios, setJuego, panelJuego,setCargado,preguntasValidas,setPreguntasValidas)=>{
+const cargarEjercicios2 = (
+  ejercicios,
+  setJuego,
+  panelJuego,
+  setCargado,
+  preguntasValidas,
+  setPreguntasValidas
+) => {
+  //los ejercicios ya están cargando desde la vista anterior, solo se necesita una estructura que almacene los ejercicios de forma con componente
+  // para esto se iterra atravez de ejercicios y en una variable llamada Juego se guardan los ejercicios en forma de componentes
 
-    //los ejercicios ya están cargando desde la vista anterior, solo se necesita una estructura que almacene los ejercicios de forma con componente
-    // para esto se iterra atravez de ejercicios y en una variable llamada Juego se guardan los ejercicios en forma de componentes
+  //finalmente iteramos los ejercicios, preguntamos el tipo, cargamos la variable juego con el tipo de juego correspondiente y una vez se cargan todos los juegos
+  //se actualiza el estado de cargando.
 
-    //finalmente iteramos los ejercicios, preguntamos el tipo, cargamos la variable juego con el tipo de juego correspondiente y una vez se cargan todos los juegos
-    //se actualiza el estado de cargando.
+  //De esta manera la renderización del mensaje con estado cargando se va a mostrar hasta que se carguen todos los ejercicios.
+  let contador = 0;
+  ejercicios.map((ejercicio, index) => {
+     /* if (ejercicio.type === "completar_texto") {
+      setJuego((juego) => [
+        ...juego,
+        <CompletarTexto key={index} ejercicio={ejercicio} miref={panelJuego} />,
+      ]);
+      contador++;
+    } else if (ejercicio.type === "ordenar") {
+      setJuego((juego) => [
+        ...juego,
+        <Arrastrar key={index} ejercicio={ejercicio} miref={panelJuego} />,
+      ]);
+      contador++;
+    } else  */if (ejercicio.type === "true_false" ) {
+      setJuego((juego) => [
+        ...juego,
+        <VerdaderoFalso key={index} ejercicio={ejercicio} miref={panelJuego} />,
+      ]);
+      contador++;
+    } else  if (ejercicio.type === "opcion_correcta_1") {
+      setJuego((juego) => [
+        ...juego,
+        <OpcionCorrecta_1
+          key={index}
+          ejercicio={ejercicio}
+          miref={panelJuego}
+        />,
+      ]);
+      contador++;
+    }  else if (ejercicio.type === "opcion_correcta_n" ) {
+        console.log("este:  ", ejercicio)
+      setJuego((juego) => [
+        ...juego,
+        <OpcionCorrecta_n
+          key={index}
+          ejercicio={ejercicio}
+          miref={panelJuego}
+        />,
+      ]);
+      contador++;
+    } else if (ejercicio.type === "emparejar" ) {
+      setJuego((juego) => [
+        ...juego,
+        <Emparejar key={index} ejercicio={ejercicio} miref={panelJuego} />,
+      ]);
+      contador++;
+    } 
+  });
 
-    //De esta manera la renderización del mensaje con estado cargando se va a mostrar hasta que se carguen todos los ejercicios.
-    let contador = 0;
-    ejercicios.map((ejercicio, index) => {
-        
-
-        // if(ejercicio.type === "completar_texto" && !ejercicio.img){
-        //     setJuego(juego => [...juego, <CompletarTexto key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-        //     contador++;
-        // }else if(ejercicio.type === "ordenar" && !ejercicio.img){
-        //     setJuego(juego => [...juego, <Arrastrar key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-        //     contador++;
-        // }else if(ejercicio.type === "true_false" && !ejercicio.img){
-        //     setJuego(juego => [...juego, <VerdaderoFalso key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-        //     contador++;
-        // }else if(ejercicio.type === "opcion_correcta_1" && !ejercicio.img){
-        //     setJuego(juego => [...juego, <OpcionCorrecta_1 key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-        //     contador++;
-        // }else if(ejercicio.type === "opcion_correcta_n" && !ejercicio.img){
-        //     setJuego(juego => [...juego, <OpcionCorrecta_n key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-        //     contador++;
-        // }else if(ejercicio.type === "emparejar"  && !ejercicio.img){
-        //     setJuego(juego => [...juego, <Emparejar key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-        //     contador++;
-        // }
-
-        if(ejercicio.type === "emparejar"  && !ejercicio.img){
-                setJuego(juego => [...juego, <Emparejar key={index} ejercicio={ejercicio} miref={panelJuego}/>])
-                contador++;
-            }
-    
-    }
-    )
-    
-    setPreguntasValidas(contador);
-    setCargado(false);
-
-}
+  setPreguntasValidas(contador);
+  setCargado(false);
+};
--- src/components/pages/ejercicios/OpcionCorrecta_1.js
+++ src/components/pages/ejercicios/OpcionCorrecta_1.js
@@ -1,9 +1,7 @@
-
 import React, { createRef, useRef } from "react";
 import { useHistory } from "react-router-dom";
 
-
-import "../../pages/ejercicios/CheckExercise.css"
+import "../../pages/ejercicios/CheckExercise.css";
 
 export const OpcionCorrecta_1 = (props) => {
   const history = useHistory();
@@ -20,11 +18,9 @@ export const OpcionCorrecta_1 = (props) => {
         ref.current.classList.contains("activado") &&
           ref.current.classList.remove("activado");
         imagenRef.current.setAttribute("aria-checked", "false");
-        if(speechSynthesis.speaking){
+        if (speechSynthesis.speaking) {
           speechSynthesis.cancel();
         }
-        
-
       });
 
       imagenRef.current.classList.toggle("activado");
@@ -32,46 +28,56 @@ export const OpcionCorrecta_1 = (props) => {
       setTimeout(() => {
         SpeechReader(imagenRef.current.children[0].innerText);
       }, 50);
-        
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="flex flex-col flex-wrap">
-      <h2 className="mt-10 text-sm mr-8 ml-8 md:text-2xl font-bold ">{(props.ejercicio.question)}</h2>
-    <div className="container m-auto p-auto w-auto">
-      <div
-        className="flex flex-wrap items-center justify-center gap-2"
-        aria-label="choice"
-        role="radiogroup"
-        ref={opciones}
-        ref={props.miref}
-      >
-        {props.ejercicio.options.map((ejercicio, index) => {
-          return (
-            <Texto
-              key={index}
-              src={
-                "https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
-              }
-              alt={"agua"}
-              nombre={"agua"}
-              marcar={marcar}
-              myref={imagesRef.current[index]}
-              data={props.ejercicio.options[index]}
-              ejercicio={props.ejercicio}
-            />
-          );
-        })}
+    <div className="flex flex-col space-y-10">
+      <h2 className="mt-10 text-sm mr-8 ml-8 md:text-2xl font-bold ">
+        {props.ejercicio.question}
+      </h2>
+
+      {props.ejercicio.img && (
+        <div className="m-auto">
+          <img
+            src={`https://drive.google.com/uc?export=view&id=${props.ejercicio.img}`}
+            width="400px"
+          ></img>
+        </div>
+      )}
+      {!props.ejercicio.img && <div className="h-20"></div>}
+      <div className="container w-auto ">
+        <div
+          className="flex flex-wrap items-center justify-center gap-2"
+          aria-label="choice"
+          role="radiogroup"
+          ref={opciones}
+          ref={props.miref}
+        >
+          {props.ejercicio.options.map((ejercicio, index) => {
+            return (
+              <Texto
+                key={index}
+                src={
+                  "https://d2pur3iezf4d1j.cloudfront.net/images/18a521f1507cb86689faa5b2e8277703"
+                }
+                alt={"agua"}
+                nombre={"agua"}
+                marcar={marcar}
+                myref={imagesRef.current[index]}
+                data={props.ejercicio.options[index]}
+                ejercicio={props.ejercicio}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
 const Texto = (props) => {
-
   return (
     <div
       style={{ height: "70px" }}
@@ -81,10 +87,8 @@ const Texto = (props) => {
       tabIndex="-1"
       ref={props.myref}
     >
-      {/* <img src={`https://drive.google.com/uc?export=view&id=${props.ejercicio.img}`}></img> */}
-      
-      
-      <button className="h-full"
+      <button
+        className="h-full"
         onClick={() => {
           props.marcar(props.myref);
         }}
@@ -95,16 +99,14 @@ const Texto = (props) => {
   );
 };
 
-
-function SpeechReader (texto){
-  let voices =  window.speechSynthesis.getVoices();
+function SpeechReader(texto) {
+  let voices = window.speechSynthesis.getVoices();
   //englich voice
-  let voice = voices.filter(voice => voice.lang === 'en-US')[0];
+  let voice = voices.filter((voice) => voice.lang === "en-US")[0];
 
-  
   let utterance = new SpeechSynthesisUtterance(texto);
   utterance.voice = voice;
   utterance.pitch = 1;
   utterance.rate = 0.8;
-  window.speechSynthesis.speak(utterance);   
+  window.speechSynthesis.speak(utterance);
 }
