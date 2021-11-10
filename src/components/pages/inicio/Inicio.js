11a9be6c91e70ecfb5e4addce493acbf86d9088a
xaviermaster <xavier.velez3132@gmail.com>
2021-11-10 00:47:14 -0500

    reduccion inicio

--- src/components/pages/inicio/Inicio.js
+++ src/components/pages/inicio/Inicio.js
@@ -18,6 +18,7 @@ import writingimg from "../../../assets/icons/Writing.png";
 import loading from "../../../assets/resource/loading.svg";
 import { api_url } from "../../../api.config";
 import shortid from "shortid";
+import Progreso from "./Progreso";
 const cookies = new Cookies();
 
 export const Inicio = () => {
@@ -91,167 +92,17 @@ export const Inicio = () => {
   <div className="grid grid-cols-12 ">
     <div className="xl:col-span-9 col-span-12 justify-center">
     {userProgress.map((modulo, index) => {
-
+      
+      
       if ((index + 1) % 2 === 0) {
         return (
-          <div key={shortid.generate()}> 
-            <ModuleProgress
-            key={shortid.generate()}
-              moduleName={"Unit: " + modulo.book_info.unit}
-              percent={parseInt(
-                ((modulo.writing.user_progress +
-                  modulo.reading.user_progress +
-                  modulo.grammar.user_progress +
-                  modulo.vocabulary.user_progress) /
-                  (modulo.writing.total_task +
-                    modulo.reading.total_task +
-                    modulo.grammar.total_task +
-                    modulo.vocabulary.total_task)) *
-                  100
-              )}
-            ></ModuleProgress>
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/writing`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/writing/${modulo.book_info.unit}/${modulo.writing.task_id}`}
-              taskid={modulo.writing.task_id}
-              percent={parseInt(
-                (modulo.writing.user_progress /
-                  modulo.writing.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"writing"}
-              img={writingimg}
-            />
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/vocabulary`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/vocabulary/${modulo.book_info.unit}/${modulo.vocabulary.task_id}`}
-              taskid={modulo.vocabulary.task_id}
-              percent={parseInt(
-                (modulo.vocabulary.user_progress /
-                  modulo.vocabulary.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"vocabulary"}
-              img={vocabularyimg}
-            />
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/reading`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/reading/${modulo.book_info.unit}/${modulo.reading.task_id}`}
-              taskid={modulo.reading.task_id}
-              percent={parseInt(
-                (modulo.reading.user_progress /
-                  modulo.reading.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"reading"}
-              img={readingimg}
-            />
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/grammar`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/grammar/${modulo.book_info.unit}/${modulo.grammar.task_id}`}
-              taskid={modulo.grammar.task_id}
-              percent={parseInt(
-                (modulo.grammar.user_progress /
-                  modulo.grammar.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"grammar"}
-              img={grammarimg}
-            />
-          </div>
-        );
+          <Progreso key={shortid.generate()} modulo = {modulo} writingimg = {writingimg} vocabularyimg = {vocabularyimg} readingimg = {readingimg} grammarimg = {grammarimg}/>
+        )
       } else {
         return (
           <div key={shortid.generate()}>
             <h2  key={shortid.generate()} className="text-2xl text-left text-green-600 mt-5 mx-10 font-bold">{`Module ${modulo.book_info.module}`}</h2>
-            <ModuleProgress
-            key={shortid.generate()}
-              moduleName={"Unit: " + modulo.book_info.unit}
-              percent={parseInt(
-                ((modulo.writing.user_progress +
-                  modulo.reading.user_progress +
-                  modulo.grammar.user_progress +
-                  modulo.vocabulary.user_progress) /
-                  (modulo.writing.total_task +
-                    modulo.reading.total_task +
-                    modulo.grammar.total_task +
-                    modulo.vocabulary.total_task)) *
-                  100
-              )}
-            ></ModuleProgress>
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/writing`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/writing/${modulo.book_info.unit}/${modulo.writing.task_id}`}
-              ruta2 = {`/modulo/${modulo.book_info.module}/writing/${modulo.book_info.unit}`}
-              taskid={modulo.writing.task_id}
-              percent={parseInt(
-                (modulo.writing.user_progress /
-                  modulo.writing.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"writing"}
-              img={writingimg}
-            />
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/vocabulary`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/vocabulary/${modulo.book_info.unit}/${modulo.vocabulary.task_id}`}
-              taskid={modulo.vocabulary.task_id}
-              percent={parseInt(
-                (modulo.vocabulary.user_progress /
-                  modulo.vocabulary.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"vocabulary"}
-              img={vocabularyimg}
-            />
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/reading`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/reading/${modulo.book_info.unit}/${modulo.reading.task_id}`}
-              taskid={modulo.reading.task_id}
-              percent={parseInt(
-                (modulo.reading.user_progress /
-                  modulo.reading.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"reading"}
-              img={readingimg}
-            />
-
-            <Activity
-              rutaReview={`/review/${modulo.book_info.book}/${modulo.book_info.module}/${modulo.book_info.unit}/grammar`}
-              moduleName={`Module: ${modulo.book_info.module}`}
-              ruta={`/modulo/${modulo.book_info.module}/grammar/${modulo.book_info.unit}/${modulo.grammar.task_id}`}
-              taskid={modulo.grammar.task_id}
-              percent={parseInt(
-                (modulo.grammar.user_progress /
-                  modulo.grammar.total_task) *
-                  100
-              )}
-              key={shortid.generate()}
-              name={"grammar"}
-              img={grammarimg}
-            />
+            <Progreso key={shortid.generate()} modulo = {modulo} writingimg = {writingimg} vocabularyimg = {vocabularyimg} readingimg = {readingimg} grammarimg = {grammarimg}/>            
           </div>
         );
       }
