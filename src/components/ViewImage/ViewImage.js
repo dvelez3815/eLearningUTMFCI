b14aaa110e55c652017409731e17d4e2fba30827
nicromano <ljose297@gmail.com>
2024-01-18 09:17:29 -0500

    Actualizando vizualización de imagenes

--- src/components/ViewImage/ViewImage.js
+++ src/components/ViewImage/ViewImage.js
@@ -3,21 +3,18 @@ import React, { Component } from "react";
 
 import {mostrarImagen} from '../Alert/Alerts';
 class Viewimage extends Component {
-  useEffec = async() => {
-
-  };
-
+  
   
 
   render() {
     return (
-      
+    
       <div className="m-auto my-6">
-        <div className="hidden">
-          <img src={`https://drive.google.com/uc?export=view&id=${this.props.img}`} alt="imagen_res"/>
-        </div>
-        
-        <button onClick={( img)=>mostrarImagen(`https://drive.google.com/uc?export=view&id=${this.props.img}` )}
+        <div className="">
+          {/* <img src={`https://drive.google.com/file/d/${this.props.img}/view`} alt="imagen_res"/> */}
+           </div>
+        {console.log(this.props.img)}    
+         <button onClick={( img)=>mostrarImagen(this.props.img)} 
           className="p-3 text-xs sm:text-lg  text-yellow-600 transition-colors duration-150 border border-yellow-300 bg-yellow-100 rounded-lg focus:shadow-outline "
         >
           view image
