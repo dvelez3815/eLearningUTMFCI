1cedbb3d16b9b89364fa47b891783dc8b3d336f2
nicromano <ljose297@gmail.com>
2024-08-07 12:45:22 -0500

    Implementado formulario

---
+++ src/api/Exams.js
@@ -0,0 +1,39 @@
+export async function getExams() {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/exam?filtered=true`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            token: process.env.REACT_APP_SECRET_TOKEN,
+        },
+    });
+    const exam = await response.json();
+    if (exam.examen === 'No se pudo obtener los examenes') return null;
+    return exam.examen
+}
+export async function createExamenUsuario(exam_user) {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/user_examenes`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            token: process.env.REACT_APP_SECRET_TOKEN,
+        },
+        body: JSON.stringify(exam_user),
+    });
+    const exam = await response.json();
+    console.log(exam)
+    if (exam.examen_user === 'No se pudo crear el examen') return null;
+    return exam.examen_user
+}
+
+export async function getExamenUsuario(id) {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/user_examenes/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            token: process.env.REACT_APP_SECRET_TOKEN,
+        },
+    });
+    const exam = await response.json();
+    if (exam.examen_user === 'No se pudo obtener los examenes') return null;
+    return exam.examen_user
+}
--- src/components/pages/RegisterExam/RegisterExam.js
+++ src/components/pages/RegisterExam/RegisterExam.js
@@ -1,47 +1,95 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import LogoProvicional from "../../../assets/resource/Logo_Provicional.png";
 import Logo_ing from "../../../assets/resource/Logo_ing.png";
+import moment from "moment";
 import { Link } from "react-router-dom"
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from 'yup'
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { getUserByCedula } from "../../../api/User";
 import { getPorgressByMail } from "../../../api/Progress";
 import { mostrarExitoEditar } from "../../Alert/Alerts";
+import { createExamenUsuario, getExamenUsuario, getExams } from "../../../api/Exams";
+import 'moment/locale/es';
 
 const RegisterExam = () => {
 
 
-    /*  const formSchema = Yup.object().shape({
-         mail: Yup.string().email('El correo no es valido').required('Campo requerido'),
-         password: Yup.string()
-             .required('La contraseña es obligatoria')
-     })
-     const formOptions = { resolver: yupResolver(formSchema) } */
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const formSchema = Yup.object().shape({
+        horario: Yup.string().required('Debe seleccionar un horario'),
+        cedula: Yup.string().required('Es obligatorio ingresar y validar la cedula').matches(/^[0-9]+$/, 'La cedula solo puede contener números').min(10, 'La cedula debe tener 10 dígitos').max(10, 'La cedula debe tener 10 dígitos'),
+        validPresionado: Yup.boolean().oneOf([true], 'Debes validar la cédula')
+
+    })
+    const formOptions = {
+        resolver: yupResolver(formSchema),
+        defaultValues: {
+            validPresionado: false
+        }
+    }
+    const { register, setValue, handleSubmit, formState: { errors } } = useForm(formOptions);
     const [cedula, setCedula] = useState('');
     const [user, setUser] = useState({})
-    const [disable, setDisable]= useState(true)
+    const [examenes, setExamenes] = useState([])
+    const [cargando, setCargando] = useState(true)
+    const [sedeSelected, setSedeSelected] = useState('')
+    const [sedes, setSedes] = useState([])
+    const [horarios, setHorarios] = useState([])
+    moment.locale('es-us')
 
     const onSubmit = async (form) => {
-        console.log(form)
+        const user_examen = {
+            id_user: user._id,
+            id_examen: form.horario
+        }
+
+        try {
+            const examen_user_response = await createExamenUsuario(user_examen)
+            console.log(examen_user_response)
+            if (!examen_user_response) throw new Error("ExamenUserError: No se pudo guardar el examen");
+            mostrarExitoEditar(
+                "Exito",
+                "Se ha registrado su fecha de examen",
+                "success"
+            );
+        } catch (error) {
+            if (error.message === "Failed to fetch") {
+                mostrarExitoEditar(
+                    "Error",
+                    "No se encontró el conexión con el servidor",
+                    "error"
+                );
+                return;
+            }
+
+            if (error.message.startsWith("ExamenUserError")) {
+                mostrarExitoEditar(
+                    "Error",
+                    "No se pudo guardar el examen",
+                    "error"
+                );
+                return;
+            }
+        }
     }
     const verificar_cedula = async () => {
-        //verificar si el usuario existe en el la base de datos
         setUser({})
         try {
             const _user = await getUserByCedula(cedula);
             if (!_user) throw new Error("UserError: No existe usuario");
+            const examen_user = await getExamenUsuario(_user._id);
+            if (examen_user) throw new Error("ExamError: El usuario ya tiene un examen registrado")
             const porcentaje = await getPorgressByMail(_user.mail)
             if (!porcentaje) throw new Error("ProgressError: El usuario no tiene progreso");
             if (porcentaje < 100) throw new Error("PorcentajeError: El usuario no ha completado el simulador");
-            console.log(_user, porcentaje)
             setUser({
+                _id: _user._id,
                 name: _user.name,
                 lastname: _user.lastname,
                 mail: _user.mail
             })
+            setValue('validPresionado', true)
         } catch (error) {
             if (error.message === "Failed to fetch") {
                 mostrarExitoEditar(
@@ -56,6 +104,14 @@ const RegisterExam = () => {
                 mostrarExitoEditar(
                     "Error",
                     "No se encontró el usuario",
+                    "error"
+                );
+                return;
+            }
+            if (error.message.startsWith("ExamError")) {
+                mostrarExitoEditar(
+                    "Error",
+                    "El usuario ya tiene un examen registrado",
                     "error"
                 );
                 return;
@@ -76,18 +132,25 @@ const RegisterExam = () => {
                 );
                 return;
             }
-
-
-
         }
-
-
-
-
     }
 
+    const getExam = async () => {
+        const _examenes = await getExams();
+        setExamenes((exam) => [..._examenes])
+        const _sedes = new Set(_examenes.map((examen) => examen.lugar))
+        setSedes((sede) => [..._sedes])
+    }
+    const changeSede = (event) => {
+        setSedeSelected(event.target.value)
+        const _horarios = examenes.filter((examen) => examen.lugar === event.target.value)
+        setHorarios((horario) => [..._horarios])
+    }
 
-
+    useEffect(() => {
+        getExam()
+        setCargando(false)
+    }, [])
     return (
         <div className=" overflow-x-hidden ">
             <div className=" mx-auto z-20  flex px-5 h-auto items-center">
@@ -122,13 +185,24 @@ const RegisterExam = () => {
 
                     <div class="p-6 rounded-lg">
                         <form class="space-y-4" id="frm-registro" onSubmit={handleSubmit(onSubmit)}>
+                            <input
+                                type="hidden"
+                                {...register('validPresionado')}
+                            />
 
                             <div>
                                 <h4 class="md:text-lg text-md font-semibold mb-2 text-left">Cédula de Identidad o Documento de Identidad:</h4>
                                 <div class="flex space-x-2">
-                                    <input name="cedula" type="text" placeholder="Número de documento" onChange={(event) => setCedula(() => event.target.value)} defaultValue={cedula} class="w-full p-2 border border-gray-300 rounded-lg" />
+                                    <input name="cedula" type="text" {...register("cedula")} placeholder="Número de documento" onChange={(event) => setCedula(() => event.target.value)} defaultValue={cedula} class="w-full p-2 border border-gray-300 rounded-lg" />
                                     <button type="button" onClick={verificar_cedula} class=" bg-yellowutm p-2 px-4  text-white rounded hover:bg-yellow-400 border border-yellow-400">Buscar</button>
                                 </div>
+                                {errors.cedula && (
+                                    <p className="text-red-500 text-sm text-left">{errors.cedula.message}</p>
+                                )}
+                                {errors.validPresionado && (
+                                    <p className="text-red-500 text-sm text-left">{errors.validPresionado.message}</p>
+                                )}
+
                             </div>
                             <div class="grid grid-cols-2 gap-4 text-left">
                                 <div>
@@ -144,17 +218,49 @@ const RegisterExam = () => {
                                 <h4 class="md:text-lg text-md font-semibold mb-2 text-left">Dirección de correo electrónico:</h4>
                                 <input id="mail" name="mail" type="email" placeholder="usuario@utm.edu.ec" disabled defaultValue={user.mail} class="w-full p-2 border border-gray-300 rounded-lg" />
                             </div>
+                            {cargando ? <div>Cargando</div> :
+                                <>
+                                    <div >
+                                        <h4 class="md:text-lg text-md text-left font-semibold mb-2">Lugar:</h4>
+                                        <select id="tipo_sex" onChange={changeSede} defaultValue={sedeSelected} class="w-full p-2 border border-gray-300 rounded-lg" name="lugar">
+                                            <option value="">Seleccionar</option>
+                                            {
+                                                sedes.map((sede) => {
+                                                    return (
+                                                        <option key={sede} value={sede}>{sede}</option>
 
+                                                    )
+                                                })
+                                            }
+                                        </select>
+                                    </div>
+                                    <div class="flex flex-col">
+                                        <h4 class="md:text-lg text-md font-semibold mb-2 text-left">Fecha y Hora:</h4>
+                                        {
+                                            horarios.length !== 0 ? horarios.map((horario) => {
+                                                return (
+                                                    <div class="flex items-left mb-2" key={horario._id}>
+                                                        <input id={horario.fecha} name="fecha" type="radio" value={horario._id} class="mr-2"   {...register("horario")} />
+                                                        <label htmlFor={horario.fecha} class="text-gray-700">{moment(horario.fecha).format('dddd MMMM Do YYYY')} - {horario.hora}</label>
+                                                    </div>
+                                                )
+                                            }) : <div><h3 className="md:text-md text-sm mb-2 text-center">No hay horarios disponibles. Seleccione un lugar </h3></div>
+                                        }
+                                        {errors.horario && (
+                                            <p className="text-red-500 text-sm text-left">{errors.horario.message}</p>
+                                        )}
+                                    </div>
+                                </>
+                            }
                             <div class="hidden my-4"></div>
                             <button type="submit" class="bg-green-500 text-white py-2 px-4 rounded-lg">Enviar</button>
-                            <div class="ui error message"></div>
                         </form>
                     </div>
 
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
